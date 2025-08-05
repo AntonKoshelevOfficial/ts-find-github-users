58e5654db56eeb36b6632c0b31bd5d3ad47346f3
AntonKoshelevOfficial <antonkoshelev.gli@gmail.com>
2025-08-05 14:14:08 +0400

    added UserTitle and upgrade UserCard components

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,15 +1,34 @@
 import styles from './UserCard.module.scss';
 import { UserStat } from '../UserStat';
 import { LocalGithubUser } from '../../types';
+import {UserTitle} from "../UserTitle";
 
-interface UserCardProps extends LocalGithubUser {}
+interface UserCardProps extends LocalGithubUser {};
 
 export const UserCard = ({
+                             bio,
+                             name,
                              repos,
+                             login,
+                             avatar,
+                             created,
                              following,
                              followers,
                          }: UserCardProps) => (
     <div className={styles.userCard}>
+        <img
+            src={avatar}
+            alt={login}
+            className={styles.avatar}
+        />
+        <UserTitle
+            name={name}
+            login={login}
+            created={created}
+        />
+        <p className={`${styles.bio}${bio ? '' : `${styles.empty}`}`}>
+            {bio || 'This profile has no bio'}
+        </p>
         <UserStat
             repos={repos}
             following={following}
