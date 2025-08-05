b642a2370b6df0b3dc7db7365914bd298f69ecab
AntonKoshelevOfficial <antonkoshelev.gli@gmail.com>
2025-08-05 15:21:32 +0400

    added InfoItem and UserInfo components/complete UI

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from 'react';
-import {ReactComponent as SunIcon} from 'assets/icon-sun.svg';
-import {ReactComponent as MoonIcon} from 'assets/icon-moon.svg';
+import { ReactComponent as SunIcon } from 'assets/icon-sun.svg';
+import { ReactComponent as MoonIcon } from 'assets/icon-moon.svg';
 
 import styles from './ThemeSwitcher.module.scss';
 
